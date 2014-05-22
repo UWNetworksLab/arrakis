1b3c10afb574a1150c9d016ee1fdd9acc9964aae
woohooyeah <dorian@woohooyeah.nl>
2014-05-22 18:09:52 +0200

    Update Fields.hs
    Further changes to allow compilation under GHC 7.6

--- tools/mackerel/Fields.hs
+++ tools/mackerel/Fields.hs
@@ -121,11 +121,11 @@ make_ftype t@(TypeRef _ _) dn = Just (TN.fromRef t dn)
 -- are polymorphic so that they don't need to know how large the total
 -- load unit is (32 bits? 8 bits?) etc.
 --
-extract_mask :: (Bits a) => Rec -> Integer -> a
+extract_mask :: (Num a, Bits a) => Rec -> Integer -> a
 extract_mask f sz = 
     foldl setBit 0 (enumFromTo (fromInteger $ offset f) 
                                (fromInteger $ (offset f) + (size f) - 1))
-insert_mask :: (Bits a) => Rec -> Integer -> a
+insert_mask :: (Num a, Bits a) => Rec -> Integer -> a
 insert_mask f sz = 
     foldl complementBit (extract_mask f sz) (enumFromTo 0 (fromInteger sz - 1))
 
